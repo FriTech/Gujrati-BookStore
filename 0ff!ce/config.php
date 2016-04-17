0c03579b4b37b9d2af17a0beb444ee6938d179af
rashah91 <rashah91@gmail.com>
2016-04-18 00:58:58 +0530

    Initial Commit
    Default Opencart v 2.1.0.2 with the following changes:
    - Removed 'install' directroy
    - Renamed 'admin' directroy

---
+++ 0ff!ce/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Gujrati-BookStore/0ff!ce/');
+define('HTTP_CATALOG', 'http://localhost/Gujrati-BookStore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Gujrati-BookStore/0ff!ce/');
+define('HTTPS_CATALOG', 'http://localhost/Gujrati-BookStore/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/Gujrati-BookStore/0ff!ce/');
+define('DIR_SYSTEM', 'C:/wamp/www/Gujrati-BookStore/system/');
+define('DIR_LANGUAGE', 'C:/wamp/www/Gujrati-BookStore/0ff!ce/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/Gujrati-BookStore/0ff!ce/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/Gujrati-BookStore/system/config/');
+define('DIR_IMAGE', 'C:/wamp/www/Gujrati-BookStore/image/');
+define('DIR_CACHE', 'C:/wamp/www/Gujrati-BookStore/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/Gujrati-BookStore/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/Gujrati-BookStore/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/Gujrati-BookStore/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/Gujrati-BookStore/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/Gujrati-BookStore/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'GBS');
+define('DB_PASSWORD', 'Gbs123');
+define('DB_DATABASE', 'GBS');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
